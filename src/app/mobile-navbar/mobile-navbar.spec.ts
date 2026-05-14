d6baa0faef8414a80b8c4e1dcdb14ab4dbcb8f27
Pascal Klodel <p.klodel@pm.me>
2026-05-14 09:08:08 +0200

    feat: Created all components anew after screwing up the pull request :D

---
+++ src/app/mobile-navbar/mobile-navbar.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MobileNavbar } from './mobile-navbar';
+
+describe('MobileNavbar', () => {
+  let component: MobileNavbar;
+  let fixture: ComponentFixture<MobileNavbar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MobileNavbar],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MobileNavbar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
