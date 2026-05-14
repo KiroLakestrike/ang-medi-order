c02d29e19a5f308ecff29116a4cbc554408724cd
Pascal Klodel <p.klodel@pm.me>
2026-05-14 09:39:14 +0200

    feat: restored previous functionality after my pull request fail.

--- src/app/mobile-navbar/mobile-navbar.ts
+++ src/app/mobile-navbar/mobile-navbar.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AppConfig } from '../app-config';
 
 @Component({
   selector: 'app-mobile-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mobile-navbar.html',
   styleUrl: './mobile-navbar.scss',
 })
-export class MobileNavbar {}
+export class MobileNavbar {
+  constructor(public config: AppConfig) {}
+}
--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppConfig } from '../app-config';
+import { HandleStorageService } from '@kirolakestrike/lakestrike-services';
 
 @Component({
   selector: 'app-settings',
@@ -6,4 +8,50 @@ import { Component } from '@angular/core';
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
-export class Settings {}
+
+export class Settings {
+  constructor(
+    public config: AppConfig,
+    public storage: HandleStorageService,
+  ) {}
+
+  // TODO: Not too happy with the color and brightness changers.
+  // Need to add a way to pop the newly active from
+
+  // Color Change Settings
+  onColorChange(value: string, list: string[]) {
+
+    // we remove the given value from the array,
+    const newList = this.removeItemOnce([...list], value);
+    console.log(value);
+
+    // remove old classes
+    document.body.classList.remove(...newList);
+    document.body.classList.add(value);
+
+    // set localStorage and config to new value
+    this.storage.setString('color-mode', value);
+    this.config.colorMode.set(value);
+  }
+
+  // brightness change settings
+
+  onBrightnessChange(value: string, list: string[]) {
+    const newList = this.removeItemOnce([...list], value);
+    console.log(value);
+
+    document.body.classList.remove(...newList);
+    document.body.classList.add(value);
+
+    this.storage.setString('brightness-mode', value);
+    this.config.colorMode.set(value);
+}
+
+  private removeItemOnce(arr: string[], value: string) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+}
