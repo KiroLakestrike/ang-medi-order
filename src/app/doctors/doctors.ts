cc6767553d355f205c2b2f6e13de63ae6bfbc3d4
Pascal Klodel <p.klodel@pm.me>
2026-05-20 07:05:59 +0200

    feat: Added needed component, and model for doctor. And wrote first functions

--- src/app/doctors/doctors.ts
+++ src/app/doctors/doctors.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { DoctorCard } from './doctor-card/doctor-card';
+import { HandleStorageService, RandomGenService } from '@kirolakestrike/lakestrike-services';
+import type { DoctorItem, DoctorList } from './doctors.models';
+import { ProfileItem } from '../profiles/profiles.model';
 
 @Component({
   selector: 'app-doctors',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, DoctorCard],
   templateUrl: './doctors.html',
   styleUrl: './doctors.scss',
 })
-export class Doctors {}
+export class Doctors implements OnInit{
+  constructor(
+    public storage: HandleStorageService,
+    private randomGen: RandomGenService,
+  ) { }
+
+  doctors: DoctorList = { doctorItem: [] };
+  mode: 'normal' | 'new' = 'normal';
+
+  newDoctor: DoctorItem = this.createEmptyDoctor();
+
+  createEmptyDoctor() {
+    return {
+      uuid: '',
+      title: '',
+      lastName: '',
+      description: '',
+      gender: '',
+    
+      email: '',
+      company: '',
+    }
+  }
+
+  ngOnInit(): void {
+    this.loadDoctors();
+  }
+
+  loadDoctors(): void {
+    this.doctors = this.storage.getJson('doclist') ?? { doctorItem: [] };
+  }
+
+  reloadDoctors(): void {
+    this.loadDoctors()
+  }
+}
