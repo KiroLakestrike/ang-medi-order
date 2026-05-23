d0550a4c6be996b7bf8d932b17567f141409541d
Pascal Klodel <p.klodel@pm.me>
2026-05-23 07:30:43 +0200

    feat: Added Icon Registry service

---
+++ src/app/core/services/icon-service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconService {
+  private matIconRegistry = inject(MatIconRegistry);
+  private domSenitizer = inject(DomSanitizer);
+
+  init(): void {
+    this.register('home');
+  }
+
+  
+}
--- src/app/medications/medications.ts
+++ src/app/medications/medications.ts
@@ -3,6 +3,7 @@ import { MedicationCard } from './medication-card/medication-card';
 import { MedicationForm } from './medication-form/medication-form';
 import { MedicationItem, MedicationList } from './medication.model';
 import { HandleStorageService } from '@kirolakestrike/lakestrike-services';
+import { DoctorList } from '../doctors/doctors.models';
 
 @Component({
   selector: 'app-medications',
@@ -14,18 +15,29 @@ export class Medications implements OnInit {
   constructor(private storage: HandleStorageService) {}
   mode: 'normal' | 'modal' = 'normal';
   medicationList: MedicationList = { medicationItem: [] };
+  doctorList: DoctorList = { doctorItem: [] };
+
+  
 
   ngOnInit(): void {
     this.loadMedications();
+    this.loadDoctors();
   }
 
   loadMedications(): void {
     this.medicationList = this.storage.getJson('medlist') ?? { medicationItem: [] }
   }
 
+  loadDoctors(): void {
+    this.doctorList = this.storage.getJson('doclist') ?? { doctorItem: [] }
+  }
+
   reloadMedications(): void {
     this.loadMedications();
   }
 
+  onNewClick() {
+    this.mode = 'modal';
+  }
   
 }
